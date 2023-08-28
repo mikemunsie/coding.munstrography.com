ee1612686b1547956c5315ed50b6c3fb7d490cb6
Michael Munsie <mikemunstrocity@gmail.com>
2023-08-28 18:56:28 -0500

    Add deployment

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+npm run build
+aws s3 cp build s3://coding.munstrography.com --recursive
+aws s3 cp s3://coding.munstrography.com/index.html s3://coding.munstrography.com/index.html --metadata-directive REPLACE --cache-control max-age=0 --content-type "text/html"
